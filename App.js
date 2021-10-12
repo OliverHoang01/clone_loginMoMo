d5e318a16f00850498f92b0a5d862f1ad79da62c
Hoàng Quốc Phóng <hqphong01@gmail.com>
2021-10-12 16:50:05 +0700

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,127 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from 'expo-status-bar';//thêm thư viện thanh bar
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import Constants from 'expo-constants'; 
+import { FontAwesome5 } from '@expo/vector-icons'; //thêm thư viện icon
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style = {styles.container}>
+      {/* đổi màu thanh bar */}
+      <StatusBar style="light" />
+       {/*Phần 1: hiển thị thông tin  */}
+      <View style={styles.content}>
+          <Text style={styles.hiText}> Xin chào! </Text>
+          <Text style={styles.userText}> HOANG QUOC PHONG </Text>
+          <Text style={styles.userText}> 0365579151 </Text>
+      </View>
+      {/* Phần 2: ô nhập mật khẩu */}
+      <View style= {styles.viewPass}>
+        <FontAwesome5 name="lock" size={15} color="#929292" style={{
+          position: 'absolute',
+          top: 51,
+          left: 30,
+          zIndex: 10,
+        }}/>
+          <TextInput style={styles.inputPass}
+            maxLength = {6}
+            placeholder="Nhập mật khẩu"
+            placeholderTextColor = "#929292"
+            keyboardType="numeric"
+            secureTextEntry={true}
+          />
+      </View>
+      {/* Phần 3: Button đăng nhập,quên mật khẩu, thoát tài khoản */}
+      <View>
+          {/*phần đăng nhập  */}
+          <View style = {styles.viewButton}> 
+            <TouchableOpacity style = {styles.bLogin}> 
+            <Text style = {styles.textLogin}>ĐĂNG NHẬP</Text>
+            </TouchableOpacity>
+          </View>
+          {/* phần quên tk, thoát tk */}
+          <View style = {styles.action}> 
+            <TouchableOpacity style = {styles.bAction}> 
+              <Text style = {styles.textF}>QUÊN MẬT KHẨU</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style = {styles.bAction}> 
+              <Text style = {styles.textF}>THOÁT TÀI KHOẢN</Text>
+            </TouchableOpacity>
+          </View>
+
+      </View>
+
+    </SafeAreaView>
   );
 }
+
+const TEXT = {
+  color: '#fff',
+  textAlign: "center",
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#b0006d',
+    paddingTop: Constants.statusBarHeight ,
+  },
+  content: {
+    paddingTop: 60,
+  },
+  textWap: {
+    
+  },
+  hiText: {
+    ...TEXT,
+    fontSize: 17,
+    fontWeight: 'bold',
+    paddingBottom: 20,
+  },
+  userText: {
+    ...TEXT,
+    paddingBottom: 15,
+  },
+
+  viewPass: {
+    paddingTop: 30,
+    marginRight: 30,
+    marginLeft: 30,
+  },
+  inputPass: {
+    fontSize: 17,
+    textAlign: 'center',
+    height: 60,
+    backgroundColor: "#fff",
+    borderRadius: 30,
+  },
+  bLogin: {
+    backgroundColor: 'red' ,
+    marginTop: 15,
+    height: 50,
+    borderRadius: 25,
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  
+  viewButton: {
+    marginRight: 30,
+    marginLeft: 30,
+  },
+  textLogin: {
+    ...TEXT,
+  },
+  action: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 30,
+  },
+  bAction: {
+    marginRight: 32,
+    marginLeft: 32,
+  },
+  textF: {
+    ...TEXT,
   },
 });
