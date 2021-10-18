10d4860d0127e4f6fd301ff6f769437f57beb337
Hoàng Quốc Phóng <hqphong01@gmail.com>
2021-10-18 09:44:02 +0700

    2nd commit

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';//thêm thư viện thanh bar
 import React from 'react';
-import { SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { Keyboard, KeyboardAvoidingView, SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, TouchableWithoutFeedback, View } from 'react-native';
 import Constants from 'expo-constants'; 
 import { FontAwesome5 } from '@expo/vector-icons'; //thêm thư viện icon
 
@@ -9,49 +9,58 @@ export default function App() {
     <SafeAreaView style = {styles.container}>
       {/* đổi màu thanh bar */}
       <StatusBar style="light" />
-       {/*Phần 1: hiển thị thông tin  */}
-      <View style={styles.content}>
-          <Text style={styles.hiText}> Xin chào! </Text>
-          <Text style={styles.userText}> HOANG QUOC PHONG </Text>
-          <Text style={styles.userText}> 0365579151 </Text>
-      </View>
-      {/* Phần 2: ô nhập mật khẩu */}
-      <View style= {styles.viewPass}>
-        <FontAwesome5 name="lock" size={15} color="#929292" style={{
-          position: 'absolute',
-          top: 51,
-          left: 30,
-          zIndex: 10,
-        }}/>
-          <TextInput style={styles.inputPass}
-            maxLength = {6}
-            placeholder="Nhập mật khẩu"
-            placeholderTextColor = "#929292"
-            keyboardType="numeric"
-            secureTextEntry={true}
-          />
-      </View>
-      {/* Phần 3: Button đăng nhập,quên mật khẩu, thoát tài khoản */}
-      <View>
-          {/*phần đăng nhập  */}
-          <View style = {styles.viewButton}> 
-            <TouchableOpacity style = {styles.bLogin}> 
-            <Text style = {styles.textLogin}>ĐĂNG NHẬP</Text>
-            </TouchableOpacity>
-          </View>
-          {/* phần quên tk, thoát tk */}
-          <View style = {styles.action}> 
-            <TouchableOpacity style = {styles.bAction}> 
-              <Text style = {styles.textF}>QUÊN MẬT KHẨU</Text>
-            </TouchableOpacity>
+      <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={{flex: 1}}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      {/*đẩy content xuống để test KeyboardAvoidingView & keyboard*/}
+      <View style={{flex: 1,justifyContent:'center'}}> 
+        {/*Phần 1: hiển thị thông tin  */}
+        <View style={styles.content}>
+            <Text style={styles.hiText}> Xin chào! </Text>
+            <Text style={styles.userText}> HOANG QUOC PHONG </Text>
+            <Text style={styles.userText}> 0365579151 </Text>
+        </View>
+        {/* Phần 2: ô nhập mật khẩu */}
+        <View style= {styles.viewPass}>
+          <FontAwesome5 name="lock" size={15} color="#929292" style={{
+            position: 'absolute',
+            top: 51,
+            left: 30,
+            zIndex: 10,
+          }}/>
+            <TextInput style={styles.inputPass}
+              maxLength = {6}
+              placeholder="Nhập mật khẩu"
+              placeholderTextColor = "#929292"
+              keyboardType="numeric"
+              secureTextEntry={true}
+            />
+        </View>
+        {/* Phần 3: Button đăng nhập,quên mật khẩu, thoát tài khoản */}
+        <View>
+            {/*phần đăng nhập  */}
+            <View style = {styles.viewButton}> 
+              <TouchableOpacity style = {styles.bLogin}> 
+              <Text style = {styles.textLogin}>ĐĂNG NHẬP</Text>
+              </TouchableOpacity>
+            </View>
+            {/* phần quên tk, thoát tk */}
+            <View style = {styles.action}> 
+              <TouchableOpacity style = {styles.bAction}> 
+                <Text style = {styles.textF}>QUÊN MẬT KHẨU</Text>
+              </TouchableOpacity>
 
-            <TouchableOpacity style = {styles.bAction}> 
-              <Text style = {styles.textF}>THOÁT TÀI KHOẢN</Text>
-            </TouchableOpacity>
-          </View>
+              <TouchableOpacity style = {styles.bAction}> 
+                <Text style = {styles.textF}>THOÁT TÀI KHOẢN</Text>
+              </TouchableOpacity>
+            </View>
 
+        </View>
       </View>
-
+      </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 }
